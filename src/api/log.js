b5010b5e4d199e3d8b613e1828d92d410ad7bf6b
fine0@foxmail.com <fine0@foxmail.com>
2020-11-27 00:48:49 +0800

    优化分页

--- src/api/log.js
+++ src/api/log.js
@@ -16,19 +16,29 @@ export const getLogByPagSize = (data) => {
     })
 }
 
-
 // 根据页码值获取日志
 export const getLogByPagNumber = (data) => {
-    return request({
-        url: '/log?page=' + data,
-        method: 'GET'
-    })
+  return request({
+    url: '/log?page=' + (data - 1),
+    method: 'GET'
+  })
+}
+
+export const getLogByParams = (data) => {
+  return request({
+    url: '/log',
+    method: 'GET',
+    params: {
+      page: data.page ? data.page : 0,
+      size: data.size ? data.size : 20
+    }
+  })
 }
 // 根据id查
 export const getLogById = (data) => {
-    return request({
-        url: '/log?opid=' + data,
-        method: 'GET'
-    })
+  return request({
+    url: '/log?opid=' + data,
+    method: 'GET'
+  })
 }
 
--- src/views/log/index.vue
+++ src/views/log/index.vue
@@ -1,10 +1,13 @@
 <template>
   <div style="padding: 20px;">
-    <a-spin :spinning="spinning1">
-      <el-table :data="logList" stripe style="width: 100%; margin-top: 50px">
-        <el-table-column type="index" label="#"></el-table-column>
-        <el-table-column prop="time" label="日期" width="200">
+    <a-spin :spinning="spinning1" tip="数据加载中">
+      <el-table max-height="600" :data="logList" stripe style="width: 100%; margin-top: 50px">
+        <el-table-column label="#">
+          <template slot-scope="scope">
+            {{ scope.row.opid }}
+          </template>
         </el-table-column>
+        <el-table-column prop="time" label="日期" width="200"/>
         <el-table-column
           prop="type"
           label="类型"
@@ -13,10 +16,8 @@
             { text: '系统日志', value: '2' },
             { text: '操作日志', value: '1' },
           ]"
-        >
-        </el-table-column>
-        <el-table-column prop="description" label="内容" width="1000">
-        </el-table-column>
+        />
+        <el-table-column prop="description" label="内容" width="1000"/>
         <el-table-column label="更多">
           <template slot-scope="scope">
             <el-button
@@ -24,97 +25,100 @@
               icon="el-icon-view"
               size="mini"
               @click="open1(scope.row.opid)"
-            ></el-button>
-          </template
-          >
+            />
+          </template>
         </el-table-column>
       </el-table>
 
-      <!-- 分页 -->
-      <el-pagination
-        @size-change="handleSizeChange"
-        @current-change="handleCurrentChange"
-        :current-page="currentPage"
-        :page-sizes="[20, 25, 30, 35]"
-        :page-size="pageSize"
-        layout="total, sizes, prev, pager, next, jumper"
-        :total="total"
-      >
-      </el-pagination>
+      <center>
+        <!-- 分页 -->
+        <el-pagination
+          style="margin-top: 20px;"
+          :current-page="currentPage"
+          :page-sizes="[20, 25, 30, 35]"
+          :page-size="pageSize"
+          layout="total, sizes, prev, pager, next, jumper"
+          :total="total"
+          @size-change="handleSizeChange"
+          @current-change="handleCurrentChange"
+        />
+      </center>
     </a-spin>
     <el-dialog
       title="日志详情"
       :visible.sync="showlogdialogVisible"
       width="30%"
     >
-      <div v-html="`${currentLog.description}`"></div>
+      <div v-html="`${currentLog.description}`"/>
       <span slot="footer" class="dialog-footer">
         <el-button @click="showlogdialogVisible = false">取 消</el-button>
-        <el-button type="primary" @click="showlogdialogVisible = false"
-          >确 定</el-button
-        >
+        <el-button
+          type="primary"
+          @click="showlogdialogVisible = false"
+        >确 定</el-button>
       </span>
     </el-dialog>
   </div>
 </template>
-
 
 <script>
 import {
   getLog,
   getLogByPagSize,
   getLogByPagNumber,
-  getLogById,
-} from "@/api/log";
+  getLogById, getLogByParams
+} from '@/api/log'
 export default {
-  name: "LogIndex",
+  name: 'LogIndex',
   data() {
     return {
+      lock: 0,
       spinning1: false,
       logList: [],
-      currentPage: 0,
+      currentPage: 1,
       pageSize: 0,
       total: 0,
       showlogdialogVisible: false,
-      currentLog: {},
-    };
+      currentLog: {}
+    }
   },
   created() {
-    this.getLogList();
+    this.getLogList()
   },
   methods: {
     async getLogList() {
-      this.spinning1 = true;
-      const data = await getLog();
-      console.log(data);
-      this.logList = data.content;
-      this.currentPage = data.number + 1;
-      this.pageSize = data.numberOfElements;
-      this.total = data.totalElements;
-      this.spinning1 = false;
+      // if (this.spinning1) {
+      //   return
+      // }
+      clearTimeout(this.lock)
+      this.spinning1 = true
+      const data = await getLogByParams({ page: this.currentPage - 1, size: this.pageSize })
+      this.logList = data.content
+      this.currentPage = data.number + 1
+      this.pageSize = data.numberOfElements
+      this.total = data.totalElements
+      this.spinning1 = false
     },
     // 当页面容量改变
     async handleSizeChange(pageSize) {
-      const data = await getLogByPagSize(pageSize);
-
-      this.logList = data.content;
-      this.getLogList();
+      this.pageSize = pageSize
+      this.lock = setTimeout(this.getLogList, 200)
+      // this.getLogList()
     },
     // 当页码值改变
     async handleCurrentChange(val) {
-      const data = await getLogByPagNumber(val);
-      console.log(data);
-      this.logList = data.content;
-      this.getLogList();
+      this.currentPage = val
+      this.lock = setTimeout(this.getLogList, 200)
+      // this.getLogList()
     },
     async open1(opid) {
-      this.showlogdialogVisible = true;
-      const data = await getLogById(opid);
+      this.showlogdialogVisible = true
+      const data = await getLogById(opid)
       // console.log(data);
-      this.currentLog = data.content[0];
-    },
-  },
-};
+      this.currentLog = data.content[0]
+    }
+  }
+}
 </script>
 
 <style scoped lang="scss">
