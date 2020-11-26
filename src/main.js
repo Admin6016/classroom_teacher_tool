e146e0800f238be66e27e16c0b6e6f88e4cb2cea
fine0@foxmail.com <fine0@foxmail.com>
2020-11-26 21:29:03 +0800

    excel导入 提交

--- src/main.js
+++ src/main.js
@@ -2,9 +2,10 @@ import Vue from 'vue'
 
 import 'normalize.css/normalize.css' // A modern alternative to CSS resets
 
+import pluginImport from '@d2-projects/vue-table-import'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-import locale from 'element-ui/lib/locale/lang/en' // lang i18n
+// import locale from 'element-ui/lib/locale/lang/en' // lang i18n
 
 import '@/styles/index.scss' // global css
 import VueQuillEditor from 'vue-quill-editor'
@@ -23,7 +24,7 @@ import { Card, Icon, List, Spin, Empty, Descriptions, Avatar, Tooltip, Comment }
 import 'ant-design-vue/dist/antd.css'
 // import '@/styles/global.css'
 Vue.use(Card)
-
+Vue.use(pluginImport)
 Vue.use(Icon)
 Vue.use(List)
 Vue.use(Spin)
@@ -51,9 +52,9 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 // set ElementUI lang to EN
-Vue.use(ElementUI, { locale })
+// Vue.use(ElementUI, { locale })
 // 如果想要中文版 element-ui，按如下方式声明
-// Vue.use(ElementUI)
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
--- src/views/group/addmanyuser.vue
+++ src/views/group/addmanyuser.vue
@@ -3,21 +3,56 @@
     <center>
       <el-upload
         style="width: 100%"
-        class="upload-demo"
         drag
-        action="https://jsonplaceholder.typicode.com/posts/"
-        multiple>
-        <i class="el-icon-upload"></i>
+        :before-upload="handleUpload"
+        action="default"
+      >
+        <i class="el-icon-upload"/>
         <div class="el-upload__text">将excel文件拖到此处，或<em>点击上传</em></div>
-        <div class="el-upload__tip" slot="tip">只能上传xls/xlsx文件，且不超过500kb</div>
+        <div slot="tip" class="el-upload__tip">只能上传xls/xlsx文件，且不超过500kb</div>
+
       </el-upload>
+
+      <div style="margin-top: 15px;">
+        <el-table v-bind="table">
+          <el-table-column
+            v-for="(item, index) in table.columns"
+            :key="index"
+            :prop="item.prop"
+            :label="item.label"
+          />
+        </el-table>
+      </div>
     </center>
   </div>
 </template>
 <script>
 export default {
   data() {
-    return {}
+    return {
+      table: {
+        columns: [],
+        data: [],
+        size: 'mini',
+        stripe: true,
+        border: true
+      }
+    }
+  },
+  methods: {
+    handleUpload(file) {
+      this.$import.xlsx(file)
+        .then(({ header, results }) => {
+          this.table.columns = header.map(e => {
+            return {
+              label: e,
+              prop: e
+            }
+          })
+          this.table.data = results
+        })
+      return false
+    }
   }
 }
 </script>
